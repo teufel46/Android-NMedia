59b2fefc3d321f5feee5b70b7ad401153ed16a72
Polina Novikova <9104785163@mail.ru>
2022-02-15 18:01:31 +0300

    add adding new post

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,9 +2,11 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import ru.netology.nmedia.adaptor.PostAdaptor
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +20,11 @@ class MainActivity : AppCompatActivity() {
         val viewModel: PostViewModel by viewModels()
 
         val adaptor = PostAdaptor(
-            likeClickListener =
+            onLikeClickListener =
             {
                 viewModel.likeById(it.id)
             },
-            shareClickListener =
+            onShareClickListener =
             {
                 viewModel.shareById(it.id)
             },
@@ -32,9 +34,23 @@ class MainActivity : AppCompatActivity() {
             },
         )
 
-        binding.root.adapter = adaptor
+        binding.list.adapter = adaptor
 
         viewModel.data.observe(this, adaptor::submitList)
+
+        binding.save.setOnClickListener {
+            val text = binding.edited.text?.toString()
+            if (text.isNullOrBlank()) {
+                Toast.makeText(this, "Content empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            viewModel.changeContent(text)
+            viewModel.save()
+
+            binding.edited.clearFocus()
+            binding.edited.setText("")
+            AndroidUtils.hideKeyboard(binding.edited)
+        }
     }
 }
 
